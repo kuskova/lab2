13ce40b6508171ee1014313ff44a341082588855
zarembin <zarembin1997@mail.ru>
2015-09-16 18:21:15 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -6,7 +6,18 @@ using namespace std;
 const double PI=3.14159265358979;
 
 long double pi(double eps){
-    return 3.14;
+    double sum = 0.0;
+	for (int i = 1; 1.0 / i >= E; i += 4)
+	{   
+		sum = sum + (1.0 / i);
+		
+	}
+	for (int i = 3; 1.0 / i >= E;i += 4)
+	{
+		sum = sum - (1.0 / i);
+		
+	}
+     return 4*sum;
 }
 
 void assert(double eps){
