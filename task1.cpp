8d08918f5fc3f610b9e0a731617fb2873dec7f88
zarembin <zarembin1997@mail.ru>
2015-09-16 19:19:19 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -12,5 +12,20 @@ double my_sinus(double x, double eps){
     return res;
 }
 
+void assert(double x , double eps)
+{
+	if (fabs(sin(x) - my_sinus(x, eps)) <= eps)
+		cout << "OK" << endl;
+	else
+		cout << "FAIL: " << x << " " << eps << endl;
+}
+
 int main(){
+    assert(3.14159265358979, 0.1);
+	assert(1.57079632679, 0.001);
+	assert(0.523598775598, 0.000001);
+	assert(3.14159265358979, 0.0000001);
+	assert(2, 0.0002);
+	assert(-1, 0.000001);
+	assert(4.2, 0.00042);
 }
